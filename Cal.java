56ab15f9ec063456677272fdf5ae027fa51a3a41
Mingyu Kim <mattmingyu@gmail.com>
2020-07-24 12:47:32 +0900

    Add files via upload

---
+++ Cal.java
@@ -0,0 +1,45 @@
+package gugudan;
+
+import java.util.Scanner;
+
+public class Cal {
+	public static void main(String[] args) {
+		String math;
+		int number1, number2;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		number1 = scan.nextInt();
+		math = scan.next();
+		number2 = scan.nextInt();
+		
+		//math = "*";
+		//number1 = 100;
+		//number2 = 30;
+		//System.out.println(number1+math+number2);
+		
+		
+		while (!"=".equals(math)) {
+			if ("+".equals(math)) {
+				System.out.println("결과: "+(number1+number2));
+				//break;
+			} else if ("-".equals(math)) {
+				System.out.println("결과: "+(number1-number2));
+				//break;
+			} else if ("*".equals(math)) {
+				System.out.println("결과: "+(number1*number2));
+				//break;
+			} else if ("/".equals(math)) {
+				System.out.println("결과: "+(number1/number2));
+				//break;
+			} else {
+				System.out.println("결과: "+(number1%number2));
+				//break;
+			}
+			number1 = scan.nextInt();
+			math = scan.next();
+			number2 = scan.nextInt();
+		}
+	}
+
+}
