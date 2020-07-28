4cd91731751eab33f8cf43d5643c5442653df126
Mingyu Kim <mattmingyu@gmail.com>
2020-07-28 14:56:07 +0900

    Add files via upload

---
+++ homework0420/BallPen.java
@@ -0,0 +1,11 @@
+package PenPackage;
+
+class BallPen extends Pen {
+	private String color;
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
