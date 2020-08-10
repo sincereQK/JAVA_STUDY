ac786a9083a7583874e6aa5e8b4e1f51949e8723
Mingyu Kim <mattmingyu@gmail.com>
2020-08-10 14:29:48 +0900

    Add files via upload

---
+++ Lecture_12/CheckBoxEx.java
@@ -0,0 +1,31 @@
+//예제 11-4 : 체크박스 생성 예
+
+import javax.swing.*; 
+import java.awt.*;
+
+public class CheckBoxEx extends JFrame{ 
+	public CheckBoxEx() {
+		setTitle("체크박스 만들기 예제"); 
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout()); 
+		ImageIcon cherryIcon = new ImageIcon("images/ex11-4/cherry.jpg"); 
+		ImageIcon selectedCherryIcon = new ImageIcon("images/ex11-4/selectedCherry.jpg");
+		
+		JCheckBox apple = new JCheckBox("사과"); 
+		JCheckBox pear = new JCheckBox("배", true); 
+		JCheckBox cherry = new JCheckBox("체리", cherryIcon); 
+		cherry.setBorderPainted(true); 
+		cherry.setSelectedIcon(selectedCherryIcon);
+
+		c.add(apple); 
+		c.add(pear); 
+		c.add(cherry);
+
+		setSize(250,150); 
+		setVisible(true);
+	} 
+	public static void main(String [] args) { 
+		new CheckBoxEx(); 
+	}
+}
