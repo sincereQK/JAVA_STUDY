84f1e5a06d4cf91193f6cc21712dd6727542ddd3
Mingyu Kim <mattmingyu@gmail.com>
2020-09-13 15:50:40 +0900

    Add files via upload

---
+++ Ch6/MenuEx.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class MenuEx {
+	public MenuEx() {
+		setTitle("Menu 만들기 예제");
+		
+		
+	}
+
+	private void setTitle(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
